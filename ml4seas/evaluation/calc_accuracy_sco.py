cb2ed999f85fb84e6bc699ed1c68410549f8ccb4
Nicolas Fauchereau <Nicolas.Fauchereau@gmail.com>
2020-04-24 16:04:59 +1200

    rewrote calc_accuracy_sco.py and added docstring

--- ml4seas/evaluation/calc_accuracy_sco.py
+++ ml4seas/evaluation/calc_accuracy_sco.py
@@ -1,4 +1,28 @@
 def calc_accuracy_sco(df, tolerance=True): 
+    
+    """
+    calculates the 'SCO' accuracy, which 'optionally' allows for a 5% tolerance
+    
+    Arguments
+    ---------
+    
+    df : the pandas DataFrame containing 4 columns: 
+    
+        0: the observed category from 1 to n categories
+        1: the probability in percentage (0 to 100) for the first category
+        2: the probability in percentage (0 to 100) for the first category
+        3: the probability in percentage (0 to 100) for the first category
+        ...  
+        
+    tolerance : Boolean, whether or not to turn the tolerance on 
+    
+    Return
+    ------
+    
+    acc : the accuracy from 0 to 1 
+    
+    
+    """
 
     import numpy as np 
     import pandas as pd
@@ -6,10 +30,9 @@ def calc_accuracy_sco(df, tolerance=True):
     acc = []
     for i in range(len(df)):
         if tolerance: 
-            r = df.iloc[i, df.iloc[i,0]] == df.iloc[i,1:].max() \
-            or abs(df.iloc[i, df.iloc[i,0]]  - df.iloc[i,1:].max()) <= 5
+            r = (int(df.iloc[i,0]) == df.iloc[i,1:].argmax()) or ((df.iloc[i, df.iloc[i,1:].argmax()] - df.iloc[i,int(df.iloc[i,0])]) <= 5.)
         else: 
-            r = df.iloc[i, df.iloc[i,0]] == df.iloc[i,1:].max()
+            r = (int(df.iloc[i,0]) == df.iloc[i,1:].argmax())
         acc.append(r)
     acc = np.array(acc)
     return acc.sum() / len(acc)
